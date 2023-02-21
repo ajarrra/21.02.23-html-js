5707d4ff4db08c833c0a84e3e977e0da6ba0a9e3
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-21 15:07:37 +0100

    changed font weight for style css in js

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ console.log(example1);
 let example2 = document.querySelector("p");
 example2.textContent = "Hello world";  //изменили описание параграфа
 example2.style.color = "tomato"; //меняем цвет текста
+example2.style.fontSize = "25px"; //с большой буквы если тире 
 console.log(example2);
 
 
 let example3 = document.querySelector(".example-3");  //класс
 example3.textContent = "This is my vloggg";
+example3.style.fontWeight = "600"; 
 console.log(example3);
 
 let example4 = document.querySelector("#example-4");  //айди 
+
 example4.textContent += "I like sushiiii";//короткий варик добавления
 
 console.log(example4);
