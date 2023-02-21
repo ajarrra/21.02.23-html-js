80534d30824b6638b6b289ae99055a54f9e3dbe8
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-21 15:02:59 +0100

    change text color in javascript

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let example1 = document.querySelector("article"); //всегда пишем документ для нтмл
-console.log(example1); 
+console.log(example1);
 
 let example2 = document.querySelector("p");
 example2.textContent = "Hello world";  //изменили описание параграфа
+example2.style.color = "tomato"; //меняем цвет текста
 console.log(example2);
 
 
@@ -11,9 +12,10 @@ example3.textContent = "This is my vloggg";
 console.log(example3);
 
 let example4 = document.querySelector("#example-4");  //айди 
-example4.textContent = "I like sushiiii";
+example4.textContent += "I like sushiiii";//короткий варик добавления
+
 console.log(example4);
 
 let example5 = document.querySelector("p:nth-child(4"); //псевдо селектор
-example5.textContent = "Like like like";
-console.log (example5);
+example5.textContent = example5.textContent + "Like like like"; //старое содержимое + новый текст
+console.log(example5);
