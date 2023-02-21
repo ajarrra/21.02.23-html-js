6a0adc530a27becc993079a81abe201eb9f6fdcd
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-21 14:57:33 +0100

    added text contant for contant p

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ console.log(example2);
 
 
 let example3 = document.querySelector(".example-3");  //класс
+example3.textContent = "This is my vloggg";
 console.log(example3);
 
 let example4 = document.querySelector("#example-4");  //айди 
+example4.textContent = "I like sushiiii";
 console.log(example4);
 
 let example5 = document.querySelector("p:nth-child(4"); //псевдо селектор
+example5.textContent = "Like like like";
 console.log (example5);
