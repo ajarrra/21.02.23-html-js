125dc27ae3ead3692fb2e8e19472bc7d872edeaa
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-21 15:57:05 +0100

    document.body.append(example8);

--- script.js
+++ script.js
@@ -42,8 +42,10 @@ let example8 = document.createElement("ul");
 //добавть ul к body
 document.body.append(example8);
 //заполним li
-for( let i = 0 ; i < 10; i++){
-    console.log
+for( let i = 1 ; i <= 10; i++){
+    let listItem = document.createElement("li")
+    listItem.textContent = "example  " + i;
+    example8.append(listItem);    
 } 
 
 
