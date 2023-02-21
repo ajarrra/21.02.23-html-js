22f64aa2e30dba316e3c9b9e57bed40044b95b62
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-21 14:43:15 +0100

    deduced paragraph with id in javascript

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ console.log(example1);
 let example2 = document.querySelector("p");
 console.log(example2);
 
-let example3 = document.querySelector(".example-3");
+let example3 = document.querySelector(".example-3");  //класс
 console.log(example3);
+
+let example4 = document.querySelector("#example-4");  //айди 
+console.log(example4);
+
+
 
